16aeab861dcda9c44b212ff6bd61f921c5d25cc2
Juni Brosas <juni@plxrmedia.com>
2023-03-23 10:37:01 +0800

    add textfield

---
+++ apps/docs/stories/RHFTextField.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { useForm } from 'react-hook-form';
+
+import FormProvider, { RHFTextField } from '../../../packages/junibrosas-core/src/forms';
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: 'RHFTextField',
+  component: RHFTextField,
+} as ComponentMeta<typeof RHFTextField>;
+
+export const Primary: ComponentStory<typeof RHFTextField> = () => {
+  const methods = useForm({
+    mode: 'onChange',
+    defaultValues: {
+      fullname: '',
+    },
+  });
+
+  return (
+    <FormProvider methods={methods}>
+      <RHFTextField name="fullname" variant="outlined" label="sample" />
+    </FormProvider>
+  )
+};
